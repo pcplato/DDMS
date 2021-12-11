e324961c8dd8b4d1098d8809d40c70d207a7f460
Towhid Hasan <81054358+t0whid@users.noreply.github.com>
2021-12-11 16:33:37 +0600

    Update about.php

--- about.php
+++ about.php
@@ -12,7 +12,48 @@
     <hr>
 
     	<div class="col-sm-6">
-    		
+    		<b>Degree offers with duration:</b>
+    		<ul>
+    			<li>Four years program, B.Sc. (Hons.)</li>
+				<li>Two years program, M.Sc.(Hons.)</li>
+				<li>Minimum two years research program, M.Phil</li>
+				<li>Minimum three years research program, PhD</li>
+				<li>Professional Degree: PDG in IT (one year)</li>
+    		</ul>
+    		<p> -- Program organization: Two semesters per year
+			Undergraduate/Graduate students intake: 50</p>
+			<br>
+				<br>
+			<b>Resources:</b>
+			<ul>
+				<li>Number of faculty members: 17</li>
+				<li>Number of technical staff: 05</li>
+				<li>Internet: Department wide internet service with Wi-Fi Support</li>
+				<li>Teaching-Learning facilities</li><br>
+				<li><b>Undergraduate Laboratories:</b><br>
+					Computer Lab, Network Lab
+					Communications Lab
+					Electronics & Digital Systems Lab
+					Microprocessor Lab
+					Image Processing Lab
+					Software Engineering Lab</li>
+			</ul>
+			<br>
+				<br>
+			<b>Admission:</b><br>
+			<p>Admission process starts in early July of each year. The local student will be notified through daily newspaper. The International student have to apply through Bangladeshi mission located in their country, for further information visit https://iu.ac.bd/ or https://www.cseiu.ac.bd/</p>
+    	</div>
+    	<div class="col-sm-6">
+			<p> -- All labs and class rooms are equipped with audio-visual supports</p>
+			<br>
+			<b>Extra-curriculum activities:</b><br>
+			<ul>
+				<li>Programming contest</li>
+				<li>Project development contest</li>
+				<li>Cultural program and sports</li>
+				<li>National wide achievements in programming contest and project</li>
+				<li>development</li>
+			</ul>
 			
     	</div>
 
